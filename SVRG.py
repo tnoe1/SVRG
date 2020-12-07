360bc7844edab3a15292c0b2fec19a3b2b2a23c4
Thomas Noel <thomas.c.noel@gmail.com>
2020-12-06 22:19:23 -0800

    Started writing custom torch optimizer for SVRG

--- SVRG.py
+++ SVRG.py
@@ -5,51 +5,31 @@
 # Gradient Descent using Predictive Variance Reduction" by
 # Johnson and Zhang
 
-
-import mnist_reader
-import numpy as np
-import pandas as pd
-import torch
-import matplotlib.pyplot as plt
+import torch.optim.Optimizer
 
 
-def sgd(batch_size: int, lr_init: float = 1e-3) -> np.ndarray:
-    ''' Implements SGD using mini-batch stochastic vector.
-
-    Args:
-        batch_size -- The number of training examples used per batch
-        lr_init    -- The initial learning rate (default 1e-3)
-
-    Returns:
-        The model's weights.   
-    '''
-    pass
-
-
-def svrg(update_freq: int, learning_rate: float = 1e-3) -> np.ndarray:
+class SVRG(Optimizer):
     ''' Implements SVRG as described by Johnson and Zhang (2013).
     
-    Args:
-        update_freq -- The number of iterations between each average gradient
-                       update.
-        learning_rate -- The fixed learning rate of the SVRG algorithm
-
-    Returns:
-        The model's weights
+    Parameters:
+        params (list): An iterable of parameters to be optimized
+        update_freq (int): The number of iterations between each average
+                               gradient update.
+        lr (float): learning rate. (default 1e-3)
 
     .. R. Johnson, T. Zhang. Accelerating Stochastic Gradient Descent using
            Predictive Variance Reduction. NIPS, 2013.
     '''
+    def __init__(self, params, update_freq, lr=1e-3):
+        if lr < 0.0:
+            raise ValueError("Learning rate invalid; must be positive")
+        if update_freq < 1:
+            raise ValueError("Update frequency invalid; must be integer >= 1")
+        defaults = dict(lr=lr, update_freq=update_freq)
+        super(SVRG, self).__init__(params, defaults)
 
 
-    print("The learning rate is {}".format(learning_rate))
-    return np.array([[1, 0], [0, 1]])
+    def step(self, closure=None):
+        pass
+        
 
-
-def main():
-    X_train, y_train = mnist_reader.load_mnist('data/fashion', kind='train')
-    X_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')    
-    svrg(10)
-
-if __name__ == '__main__':
-    main()
